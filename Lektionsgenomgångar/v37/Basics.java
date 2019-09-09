c4b7b87efb3a0709b029183458152697635123dd
areehnbergntig <are.ehnberg@ntig.se>
2019-09-09 10:47:31 +0200

    Skrivit lite scannergrejer

---
+++ Lektionsgenomgångar/v37/Basics.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Basics {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hej världen");
+		
+		String str = input.nextLine();
+		
+		System.out.println(str);
+		
+	}
+	
+	
+	
+
+}
