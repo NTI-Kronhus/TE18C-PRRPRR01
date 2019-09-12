adecfdd9aa7ee86b927a13912f26c233613aef55
areehnbergntig <are.ehnberg@ntig.se>
2019-09-12 10:58:41 +0200

    Lektion tors v37

--- Lektionsgenomgångar/v37/Basics.java
+++ Lektionsgenomgångar/v37/Basics.java
@@ -4,17 +4,35 @@ public class Basics {
 	
 	public static void main(String[] args) {
 		
-		Scanner input = new Scanner(System.in);
-		
+		/*Scanner input = new Scanner(System.in);
 		System.out.println("Hej världen");
 		
 		String str = input.nextLine();
-		
 		System.out.println(str);
 		
 		String str2 = input.nextLine();
+		System.out.println(str2);*/
 		
-		System.out.println(str2);
+		
+		
+		int x = 3;
+		x = x + 1;
+		
+		int y = 5;
+		
+		x = y + x*2;
+		
+		
+		x = 17;
+		y = 73;
+		
+		int medel = (x+y)/2;
+		System.out.println(medel);
+		
+		String namn = "Are";
+		char c = 'h';
+		
+		
 		
 	}
 	
