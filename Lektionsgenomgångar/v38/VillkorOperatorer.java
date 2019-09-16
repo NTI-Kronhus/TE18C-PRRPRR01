1972e36bf7d073b2873d52d3279dce616d63e3fd
areehnbergntig <are.ehnberg@ntig.se>
2019-09-16 10:41:53 +0200

    Villkor och operatorer

---
+++ Lektionsgenomgångar/v38/VillkorOperatorer.java
@@ -0,0 +1,62 @@
+
+public class VillkorOperatorer {
+
+	public static void main(String[] args) {
+		
+		/*
+		if(villkor) {
+			gör något;
+		} 
+		
+		else if (villkor 2) {
+			gör något annat;
+		} 
+		
+		else {
+			
+		}
+		*/
+		
+		/*
+		
+		Aritmetiska Operatorer
+		
+		+ - addition
+		- - subtraktion
+		* - multiplikation
+		/ - division
+		% - modulus
+		-- - minska med ett
+		++ - öka med ett
+		 
+		 Jämförelse-operatorer
+		 
+		 > - större än
+		 < - mindre än
+		 == - lika med
+		 >= - större än eller lika med
+		 <= - mindre än eller lika med
+		 != - inte lika med
+		 
+		 Logiska operatorer
+		 
+		 && - och
+		 || - eller
+		 ! - inte
+		 
+		 */
+		
+		
+		int x = 3;
+		int y = 5;
+		
+		if(x > y) {
+			
+			System.out.println("x är störr än y");
+		} else {
+			System.out.println("x är mindre än y");
+		}
+		
+	}
+	
+}
