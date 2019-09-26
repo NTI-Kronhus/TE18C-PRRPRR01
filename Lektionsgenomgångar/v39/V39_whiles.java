a9f233a5f3a1c412dc83a82364d55336851c8c99
areehnbergntig <are.ehnberg@ntig.se>
2019-09-26 10:55:18 +0200

    Lektionsgenomgångar v39

---
+++ Lektionsgenomgångar/v39/V39_whiles.java
@@ -0,0 +1,27 @@
+
+public class V39_whiles {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {  //används om man VET hur många gånger man vill iterera
+			
+			//gör något
+		}
+		
+		
+		
+		boolean hasWon = false;
+		
+		while(!hasWon) {  //används om man INTE VET hur många gånger man vill iterera
+			
+			//gör något
+		}
+		
+
+		do {  
+			//gör något
+		} while(!hasWon);  //samma som den vanliga while-loopen men körs alltid MINST en gång
+
+	}
+
+}
